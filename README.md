8c1296d2d01b0c7630ec77ead163f6655fddda40
andrewdoss <andrewjdoss@gmail.com>
2021-08-02 11:18:17 -0700

    Cleaned up code to share

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # pi-bitdotio-air
 
-python3 -m venv venv
-source venv/bin/activate
-python3 -m pip install --upgrade pip -r requirements.txt
+A minimal air quality data logger. This can be refactored, and an alternate upload method using the REST API with Python requests added, to make a demo on using bit.io for data logging. The demo may be best with a simple dashboard connected to bit.io for live remote monitoring.
+
+## Setup
+
+- Add a .env with your own bit.io API key
+- Update the YAML to point to your desired repo/table
+- Create environment
+    - `python3 -m venv venv`<br>
+    - `source venv/bin/activate`<br>
+    - `python3 -m pip install --upgrade pip -r requirements.txt`<br>
+
+
+## Run
+`python pi_bitdotio.py`
--- pi_bitdotio_air.py
+++ pi_bitdotio_air.py
@@ -1,34 +1,23 @@
 import logging
+import os
 import serial
 import sys
 import time
 
 import bitdotio
+from dotenv import load_dotenv
+import yaml
+
+load_dotenv()
+BITDOTIO_API_KEY = os.getenv('BITDOTIO_API_KEY')
+
 
 logging.basicConfig(
+    filename='log.out',
+    filemode='a',
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
-
-CONFIG = {
-    'columns': [
-        'location',
-        'sensor_id',
-        'pm_2_5',
-        'pm_10'],
-    'measurements': {
-        'pm_2_5': (2, 2, 10),
-        'pm_10': (4, 2, 10)
-    },
-    'sensor_id': (6, 2),
-    'period': 60,
-    'location': "Andrew's house - inside",
-    'repo_owner': 'andrewdoss',
-    'repo_name': 'air_quality',
-    'table_name': 'pm_measurements'
-}
-
-ser = serial.Serial('/dev/ttyUSB0')
 
 
 def parse_value(data, start_byte, num_bytes):
@@ -43,15 +32,10 @@ def parse_measurement(data, start_byte, num_bytes, denom):
 def execute_sql(bitdotio, sql, params=None):
     '''Run arbitrary SQL statements on bitdotio'''
     try:
-        # Connect to bit.io
         conn = bitdotio.get_connection()
-        # Open cursor with bit.io server
         cur = conn.cursor()
-        # Execute sql
         cur.execute(sql, params)
-        # Close cursor
         cur.close()
-        # Commit the changes (only relevent for write ops)
         conn.commit()
     except Exception as e:
         print(e)
@@ -70,18 +54,27 @@ def insert_record(bitdotio, record, CONFIG):
     execute_sql(bitdotio, sql, record)
 
 
-b = bitdotio.bitdotio(BITDOTIO_API_KEY)
+def main():
+    with open('config.yaml', 'r') as f:
+        CONFIG = yaml.safe_load(f)
+
+    ser = serial.Serial('/dev/ttyUSB0')
+
+    b = bitdotio.bitdotio(BITDOTIO_API_KEY)
+    
+    while True:
+        sample = [ser.read(10) for i in range(CONFIG['period'])]
+
+        record = {'location': CONFIG['location']}
+
+        record['sensor_id'] = parse_value(sample[0], *CONFIG['sensor_id'])
+        for measurement, parse_args in CONFIG['measurements'].items():
+            meas_sum = sum([parse_measurement(x, *parse_args) for x in sample])
+            record[measurement] = meas_sum / CONFIG['period']
+
+        insert_record(b, [record[col] for col in CONFIG['columns']], CONFIG)
+        logging.info(f'RECORD UPLOADED: {record}')
 
 
-while True:
-    sample = [ser.read(10) for i in range(CONFIG['period'])]
-
-    record = {'location': CONFIG['location']}
-
-    record['sensor_id'] = parse_value(sample[0], *CONFIG['sensor_id'])
-    for measurement, parse_args in CONFIG['measurements'].items():
-        meas_sum = sum([parse_measurement(x, *parse_args) for x in sample])
-        record[measurement] = meas_sum / CONFIG['period']
-
-    insert_record(b, [record[col] for col in CONFIG['columns']], CONFIG)
-    logging.info(f'RECORD UPLOADED: {record}')
+if __name__ == '__main__':
+    main()
